7c745f1a49c00cd7546a98720714ad64134f5dbc
Mahmudul Hasan Mehdi <mhmehdisylhet@gmail.com>
2026-09-22 14:44:28 +0600

    Add files via upload

---
+++ Day 2/YourWork/stringcount1.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+string fullname=" Mahmudul Hasan Mehdi";
+cout<< "a appeared in fullname "<<" "<<count(fullname.begin(),fullname.end(),'a');
+
+
+}
